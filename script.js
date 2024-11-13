b4f2828f635eeb4d94a5bf94d616df6596be82be
Sumanthvu <sumanthveslavath07@gmail.com>
2024-11-13 19:45:42 +0530

    GDG Assignment

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-    // let a = document.getElementById('bid');
-
-    // async function fetchQuotes(count) {
-    //     let quotes = [];
-    //     for (let i = 0; i < count; i++) {
-    //         try {
-    //             let response = await fetch('https://dummyjson.com/quotes/random');
-    //             let quote = await response.json();
-    //             quotes.push(quote);
-    //         } catch (error) {
-    //             console.error('Error fetching quote:', error);
-    //         }
-    //     }   
-    //     return quotes;
-    // }
-
-    // function func2(quotes) {
-    //     quotes.forEach(quote => {
-    //         console.log(quote);
-    //     });
-    // }
-
-    // function func1() {
-    //     a.addEventListener('click', async () => {
-    //         const quotes = await fetchQuotes(6);
-    //         func2(quotes);
-    //     });
-    // }
-
-    // func1();
 
     let a=document.getElementById('bid');
 
